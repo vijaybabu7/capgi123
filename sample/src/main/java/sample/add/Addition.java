683cde6cf69ba4234281623bfe8be694063068ad
vivempat <vivempat@DIN78000422.corp.capgemini.com>
2019-07-04 14:54:24 +0530

    addition and subtraction

---
+++ sample/src/main/java/sample/add/Addition.java
@@ -0,0 +1,12 @@
+package sample.add;
+
+public class Addition {
+
+	public static void main(String[] args) {
+int a=10;
+int b=20;
+System.out.println("addition of a+b="+(a+b));
+
+	}
+
+}
---
+++ sample/src/main/java/sample/sub/Subtraction.java
@@ -0,0 +1,10 @@
+package sample.sub;
+
+public class Subtraction {
+
+	public static void main(String[] args) {
+int a=100,b=50;
+System.out.println("sub a-b="+(a-b));
+	}
+
+}
